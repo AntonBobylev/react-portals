0a0ead5486a887c655a3deef801698a632fd7ad9
AntonBobylev <ya-anton3632@yandex.ru>
2024-07-12 18:54:23 +0300

    patient portal grid added

--- application/src/references/patient_portal/PatientPortal.tsx
+++ application/src/references/patient_portal/PatientPortal.tsx
@@ -1,7 +1,10 @@
+import PatientPortalGrid from "./PatientPortalGrid.tsx";
+
 export default function PatientPortal() {
     return (
         <>
             <div>Welcome to the patient portal!</div>
+            <PatientPortalGrid />
         </>
     )
 };
