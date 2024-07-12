adc1d8485f464864b1288f69b50e7d24feebc620
AntonBobylev <ya-anton3632@yandex.ru>
2024-07-12 18:01:36 +0300

    frontend mass code styling

--- application/src/App.tsx
+++ application/src/App.tsx
@@ -1,9 +1,9 @@
 import './App.css'
 
 export default function App() {
-  return (
-    <>
-        <span>Welcome to the application frontend!</span>
-    </>
-  )
+    return (
+        <>
+            <span>Welcome to the application frontend!</span>
+        </>
+    )
 };
