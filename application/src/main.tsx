16869c99400e78bd830bdf8dbc12ebe90f4f4e84
AntonBobylev <ya-anton3632@yandex.ru>
2024-07-12 22:58:25 +0300

    dark theme applied for material-ui

--- application/src/main.tsx
+++ application/src/main.tsx
@@ -1,16 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import {
-    createBrowserRouter,
-    RouterProvider
-} from 'react-router-dom';
+import { createBrowserRouter, RouterProvider} from 'react-router-dom';
 
-import './index.css';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
 
 import PatientPortal from './references/patient_portal/PatientPortal.tsx';
 import Root from './routes/root.tsx';
 import ErrorPage from './error-page';
+
+import './index.css';
 
 const router = createBrowserRouter([{
     path: '/',
@@ -22,8 +21,16 @@ const router = createBrowserRouter([{
     }]
 }]);
 
+const darkTheme = createTheme({
+    palette: {
+        mode: 'dark'
+    }
+});
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
-        <RouterProvider router={router}/>
-    </React.StrictMode>,
-)
+        <ThemeProvider theme={darkTheme}>
+            <RouterProvider router={router}/>
+        </ThemeProvider>
+    </React.StrictMode>
+);
