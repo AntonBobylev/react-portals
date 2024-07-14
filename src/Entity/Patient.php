00ff2ef0e3df3ea216171b6af6639b4adc9f83cb
AntonBobylev <ya-anton3632@yandex.ru>
2024-07-14 19:50:55 +0300

    EntityInterface applied

--- src/Entity/Patient.php
+++ src/Entity/Patient.php
@@ -2,13 +2,14 @@
 
 namespace App\Entity;
 
+use App\Entity\Interfaces\EntityInterface;
 use App\Repository\PatientRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: PatientRepository::class)]
 #[ORM\Table(name: "patients")]
-class Patient
+class Patient implements EntityInterface
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
