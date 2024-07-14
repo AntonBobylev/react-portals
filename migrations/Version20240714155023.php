3e39dfe40d253719f8e7f9605711f3ddded2656f
AntonBobylev <ya-anton3632@yandex.ru>
2024-07-14 19:10:16 +0300

    cs

--- migrations/Version20240714155023.php
+++ migrations/Version20240714155023.php
@@ -19,7 +19,6 @@ final class Version20240714155023 extends AbstractMigration
 
     public function up(Schema $schema): void
     {
-        // this up() migration is auto-generated, please modify it to your needs
         $this->addSql(
             "CREATE TABLE patients (
                     id INT AUTO_INCREMENT NOT NULL,
@@ -36,7 +35,6 @@ final class Version20240714155023 extends AbstractMigration
 
     public function down(Schema $schema): void
     {
-        // this down() migration is auto-generated, please modify it to your needs
         $this->addSql("DROP TABLE patients");
     }
 }
